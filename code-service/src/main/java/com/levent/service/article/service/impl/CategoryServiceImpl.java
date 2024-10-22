48eee7e6b7f9065857553813d5e3813f9fc9966d
guanxinyi <1993262229@qq.com>
2024-10-22 19:06:53 +0800

    添加分类业务

--- code-service/src/main/java/com/levent/service/article/service/impl/CategoryServiceImpl.java
+++ code-service/src/main/java/com/levent/service/article/service/impl/CategoryServiceImpl.java
@@ -1,38 +1,99 @@
 package com.levent.service.article.service.impl;
 
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.levent.api.model.enums.YesOrNoEnum;
 import com.levent.api.model.vo.article.dto.CategoryDTO;
+import com.levent.service.article.conveter.ArticleConverter;
+import com.levent.service.article.repository.dao.CategoryDao;
+import com.levent.service.article.repository.entity.CategoryDO;
 import com.levent.service.article.service.CategoryService;
-import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
-*@Author: guanxinyi
-*@CreateTime: 2024-10-20
-*@Description: 类目服务
-*@Version: 1.0
-*/
-@Slf4j
+ * 类目Service
+ *
+ * @author louzai
+ * @date 2022-07-20
+ */
 @Service
 public class CategoryServiceImpl implements CategoryService {
-    @Override
-    public String queryCategoryName(Long categoryId) {
-        return "";
+    /**
+     * 分类数一般不会特别多，如编程领域可以预期的分类将不会超过30，所以可以做一个全量的内存缓存
+     * todo 后续可改为Guava -> Redis
+     */
+    private LoadingCache<Long, CategoryDTO> categoryCaches;
+
+    private CategoryDao categoryDao;
+
+    public CategoryServiceImpl(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
     }
 
+    @PostConstruct
+    public void init() {
+        categoryCaches = CacheBuilder.newBuilder().maximumSize(300).build(new CacheLoader<Long, CategoryDTO>() {
+            @Override
+            public CategoryDTO load(@NotNull Long categoryId) throws Exception {
+                CategoryDO category = categoryDao.getById(categoryId);
+                if (category == null || category.getDeleted() == YesOrNoEnum.YES.getCode()) {
+                    return CategoryDTO.EMPTY;
+                }
+                return new CategoryDTO(categoryId, category.getCategoryName(), category.getRank());
+            }
+        });
+    }
+
+    /**
+     * 查询类目名
+     *
+     * @param categoryId
+     * @return
+     */
+    @Override
+    public String queryCategoryName(Long categoryId) {
+        return categoryCaches.getUnchecked(categoryId).getCategory();
+    }
+
+    /**
+     * 查询所有的分类
+     *
+     * @return
+     */
     @Override
     public List<CategoryDTO> loadAllCategories() {
-        return List.of();
+        if (categoryCaches.size() <= 5) {
+            refreshCache();
+        }
+        List<CategoryDTO> list = new ArrayList<>(categoryCaches.asMap().values());
+        list.removeIf(s -> s.getCategoryId() <= 0);
+        list.sort(Comparator.comparingInt(CategoryDTO::getRank));
+        return list;
+    }
+
+    /**
+     * 刷新缓存
+     */
+    @Override
+    public void refreshCache() {
+        List<CategoryDO> list = categoryDao.listAllCategoriesFromDb();
+        categoryCaches.invalidateAll();
+        categoryCaches.cleanUp();
+        list.forEach(s -> categoryCaches.put(s.getId(), ArticleConverter.toDto(s)));
     }
 
     @Override
     public Long queryCategoryId(String category) {
-        return 0L;
-    }
-
-    @Override
-    public void refreshCache() {
-
+        return categoryCaches.asMap().values().stream()
+                .filter(s -> s.getCategory().equalsIgnoreCase(category))
+                .findFirst().map(CategoryDTO::getCategoryId).orElse(null);
     }
 }
--- code-service/src/main/java/com/levent/service/article/service/impl/CategorySettingServiceImpl.java
+++ code-service/src/main/java/com/levent/service/article/service/impl/CategorySettingServiceImpl.java
@@ -1,39 +1,75 @@
 package com.levent.service.article.service.impl;
 
+
 import com.levent.api.model.vo.PageVo;
 import com.levent.api.model.vo.article.CategoryReq;
 import com.levent.api.model.vo.article.SearchCategoryReq;
 import com.levent.api.model.vo.article.dto.CategoryDTO;
+import com.levent.core.util.NumUtil;
+import com.levent.service.article.conveter.CategoryStructMapper;
+import com.levent.service.article.repository.dao.CategoryDao;
+import com.levent.service.article.repository.entity.CategoryDO;
+import com.levent.service.article.repository.params.SearchCategoryParams;
+import com.levent.service.article.service.CategoryService;
 import com.levent.service.article.service.CategorySettingService;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
- * @Author: guanxinyi
- * @CreateTime: 2024-10-20
- * @Description: 类目设置服务
- * @Version: 1.0
+ * 分类后台接口
+ *
+ * @author louzai
+ * @date 2022-09-17
  */
-@Slf4j
 @Service
 public class CategorySettingServiceImpl implements CategorySettingService {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @Autowired
+    private CategoryService categoryService;
+
     @Override
     public void saveCategory(CategoryReq categoryReq) {
-        
+        CategoryDO categoryDO = CategoryStructMapper.INSTANCE.toDO(categoryReq);
+        if (NumUtil.nullOrZero(categoryReq.getCategoryId())) {
+            categoryDao.save(categoryDO);
+        } else {
+            categoryDO.setId(categoryReq.getCategoryId());
+            categoryDao.updateById(categoryDO);
+        }
+        categoryService.refreshCache();
     }
 
     @Override
     public void deleteCategory(Integer categoryId) {
-
+        CategoryDO categoryDO = categoryDao.getById(categoryId);
+        if (categoryDO != null){
+            categoryDao.removeById(categoryDO);
+        }
+        categoryService.refreshCache();
     }
 
     @Override
     public void operateCategory(Integer categoryId, Integer pushStatus) {
-
+        CategoryDO categoryDO = categoryDao.getById(categoryId);
+        if (categoryDO != null){
+            categoryDO.setStatus(pushStatus);
+            categoryDao.updateById(categoryDO);
+        }
+        categoryService.refreshCache();
     }
 
     @Override
-    public PageVo<CategoryDTO> getCategoryList(SearchCategoryReq params) {
-        return null;
+    public PageVo<CategoryDTO> getCategoryList(SearchCategoryReq req) {
+        // 转换
+        SearchCategoryParams params = CategoryStructMapper.INSTANCE.toSearchParams(req);
+        // 查询
+        List<CategoryDTO> categoryDTOS = categoryDao.listCategory(params);
+        Long totalCount = categoryDao.countCategory(params);
+        return PageVo.build(categoryDTOS, params.getPageSize(), params.getPageNum(), totalCount);
     }
 }
