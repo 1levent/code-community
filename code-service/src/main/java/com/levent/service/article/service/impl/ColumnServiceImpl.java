8dbf779f55e7a0acd2eb5bbb6032982a5bcd274e
guanxinyi <1993262229@qq.com>
2024-10-22 19:09:43 +0800

    添加专栏业务

--- code-service/src/main/java/com/levent/service/article/service/impl/ColumnServiceImpl.java
+++ code-service/src/main/java/com/levent/service/article/service/impl/ColumnServiceImpl.java
@@ -1,57 +1,124 @@
 package com.levent.service.article.service.impl;
 
+
+import com.levent.api.model.exception.ExceptionUtil;
 import com.levent.api.model.vo.PageListVo;
 import com.levent.api.model.vo.PageParam;
 import com.levent.api.model.vo.article.dto.ColumnDTO;
 import com.levent.api.model.vo.article.dto.SimpleArticleDTO;
+import com.levent.api.model.vo.constants.StatusEnum;
+import com.levent.api.model.vo.user.dto.BaseUserInfoDTO;
+import com.levent.api.model.vo.user.dto.ColumnFootCountDTO;
+import com.levent.service.article.conveter.ColumnConvert;
+import com.levent.service.article.repository.dao.ArticleDao;
+import com.levent.service.article.repository.dao.ColumnArticleDao;
+import com.levent.service.article.repository.dao.ColumnDao;
 import com.levent.service.article.repository.entity.ColumnArticleDO;
+import com.levent.service.article.repository.entity.ColumnInfoDO;
 import com.levent.service.article.service.ColumnService;
-import lombok.extern.slf4j.Slf4j;
+import com.levent.service.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * @Author: guanxinyi
- * @CreateTime: 2024-10-20
- * @Description: 栏目服务
- * @Version: 1.0
+ * @author YiHui
+ * @date 2022/9/14
  */
-@Slf4j
 @Service
 public class ColumnServiceImpl implements ColumnService {
+    @Autowired
+    private ColumnDao columnDao;
+    @Autowired
+    private ArticleDao articleDao;
+
+    @Autowired
+    private ColumnArticleDao columnArticleDao;
+
+    @Autowired
+    private UserService userService;
+
     @Override
     public ColumnArticleDO getColumnArticleRelation(Long articleId) {
-        return null;
+        return columnArticleDao.selectColumnArticleByArticleId(articleId);
     }
 
+    /**
+     * 专栏列表
+     *
+     * @return
+     */
     @Override
     public PageListVo<ColumnDTO> listColumn(PageParam pageParam) {
-        return null;
-    }
-
-    @Override
-    public ColumnArticleDO queryColumnArticle(long columnId, Integer order) {
-        return null;
+        List<ColumnInfoDO> columnList = columnDao.listOnlineColumns(pageParam);
+        List<ColumnDTO> result = columnList.stream().map(this::buildColumnInfo).collect(Collectors.toList());
+        return PageListVo.newVo(result, pageParam.getPageSize());
     }
 
     @Override
     public ColumnDTO queryBasicColumnInfo(Long columnId) {
-        return null;
+        // 查找专栏信息
+        ColumnInfoDO column = columnDao.getById(columnId);
+        if (column == null) {
+            throw ExceptionUtil.of(StatusEnum.COLUMN_NOT_EXISTS, columnId);
+        }
+        return ColumnConvert.toDto(column);
     }
 
     @Override
     public ColumnDTO queryColumnInfo(Long columnId) {
-        return null;
+        return buildColumnInfo(queryBasicColumnInfo(columnId));
+    }
+
+    private ColumnDTO buildColumnInfo(ColumnInfoDO info) {
+        return buildColumnInfo(ColumnConvert.toDto(info));
+    }
+
+    /**
+     * 构建专栏详情信息
+     *
+     * @param dto
+     * @return
+     */
+    private ColumnDTO buildColumnInfo(ColumnDTO dto) {
+        // 补齐专栏对应的用户信息
+        BaseUserInfoDTO user = userService.queryBasicUserInfo(dto.getAuthor());
+        dto.setAuthorName(user.getUserName());
+        dto.setAuthorAvatar(user.getPhoto());
+        dto.setAuthorProfile(user.getProfile());
+
+        // 统计计数
+        ColumnFootCountDTO countDTO = new ColumnFootCountDTO();
+        // 更新文章数
+        countDTO.setArticleCount(columnDao.countColumnArticles(dto.getColumnId()));
+        // 专栏阅读人数
+        countDTO.setReadCount(columnDao.countColumnReadPeoples(dto.getColumnId()));
+        // 总的章节数
+        countDTO.setTotalNums(dto.getNums());
+        dto.setCount(countDTO);
+        return dto;
+    }
+
+
+    @Override
+    public ColumnArticleDO queryColumnArticle(long columnId, Integer section) {
+        ColumnArticleDO article = columnDao.getColumnArticleId(columnId, section);
+        if (article == null) {
+            throw ExceptionUtil.of(StatusEnum.ARTICLE_NOT_EXISTS, section);
+        }
+        return article;
     }
 
     @Override
     public List<SimpleArticleDTO> queryColumnArticles(long columnId) {
-        return List.of();
+        return columnDao.listColumnArticles(columnId);
     }
 
     @Override
     public Long getTutorialCount() {
-        return 0L;
+        return this.columnDao.countColumnArticles();
     }
+
 }
